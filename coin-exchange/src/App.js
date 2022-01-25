acdde15c5038d98ab11592f13b9c4165f50faf6e
alpdevcode <alp.rgorpilla@gmail.com>
2022-01-25 18:56:49 +0800

    Add button and event handler to refresh price
    Add button and corresponding event handler to randomly refresh price

--- coin-exchange/src/App.js
+++ coin-exchange/src/App.js
@@ -16,7 +16,7 @@ function App() {
           <tr>
             <td>Name</td>
             <td>Ticker</td>
-            <td>Price</td>
+            <td>Price($)</td>
           </tr>
         </thead>
         <tbody>
--- coin-exchange/src/components/Coin/Coin.jsx
+++ coin-exchange/src/components/Coin/Coin.jsx
@@ -3,12 +3,40 @@ import './Coin.css';
 import PropTypes from 'prop-types';
 
 export default class Coin extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      price: this.props.price
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    
+    const randomPercent = 0.99 + Math.random() * 0.01;
+    this.setState(
+      (oldState) => {
+        return {
+          price: oldState.price * randomPercent
+        }
+      }
+    );
+  }
+
   render() {
     return (        
         <tr className="row-coin">
             <td>{this.props.name}</td>
             <td>{this.props.ticker}</td>
-            <td>$ {this.props.price}</td> 
+            <td>{this.state.price}</td>
+            <td>
+              <form action="#" method="post">
+                <button onClick={this.handleClick}>Refresh</button>
+              </form>
+            </td>
        </tr>
     );
   }
