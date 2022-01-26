16212a5db05be9c3a07ac950b56ff2b7b858594d
alpdevcode <alp.rgorpilla@gmail.com>
2022-01-26 22:54:05 +0800

    Create separate components for the header and Coin list
    Create separate components for the header and Coin list. Move corresponding styling and apply prop drilling

---
+++ coin-exchange/src/components/CoinList/CoinList.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Coin from '../Coin/Coin';
+import styled from 'styled-components';
+
+const TableCoin = styled.table`
+  margin: 50px auto 50px auto;
+  display: inline-block;
+  font-size: 1.4rem;
+`;
+
+export default class CoinList extends Component {
+  render() {
+    return (
+      <TableCoin>
+        <thead>
+          <tr>
+          <td>Name</td>
+          <td>Ticker</td>
+          <td>Price($)</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            this.props.coinData.map(
+               ({name, ticker, price}) => 
+               <Coin key={ticker} name={name} ticker={ticker} price={price} />
+            )
+          }
+        </tbody>
+      </TableCoin>
+    )
+  }
+}
